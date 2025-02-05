7fc60db6241811e71701b2023a9a968bb7be608f
Enyagam Churchill Chukwunonso <churchill.chukwunonso.cc@gmail.com>
2025-02-05 17:08:10 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Studen ID: 101465292</h1>
+      <h1>Studen ID: Churchill Enyagam</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Churchill Enyagam
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
